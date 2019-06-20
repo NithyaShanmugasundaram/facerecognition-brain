ea237eb3abb58fceee518845609038c9aa979114
Ganesh kumar M <gunz@Ganeshs-MacBook-Pro.local>
2019-06-20 13:50:19 +0800

    init

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navigation = () => {
+    
+      return (
+        <nav style={{display: 'flex', justifyContent: 'center'}}>
+          <p  className='f2 bold dim black  pa3 pointer'>Face Recognition App</p>
+        </nav>
+      );
+    
+    }
+
+
+export default Navigation;
