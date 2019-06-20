dbd7e12709bff80c8efb33b1b9a1654d55439143
Ganesh kumar M <gunz@Ganeshs-MacBook-Pro.local>
2019-06-20 13:52:33 +0800

    Updates

---
+++ precache-manifest.c2a5a1449690221cb178a9eb82edd785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48a8b3801e470db7f9f81587bdf26d77",
+    "url": "/facerecognition-brain/index.html"
+  },
+  {
+    "revision": "ce1e90f5a5b570ed8a30",
+    "url": "/facerecognition-brain/static/css/2.9878de88.chunk.css"
+  },
+  {
+    "revision": "66a67ce241b04d832705",
+    "url": "/facerecognition-brain/static/css/main.b80b51af.chunk.css"
+  },
+  {
+    "revision": "ce1e90f5a5b570ed8a30",
+    "url": "/facerecognition-brain/static/js/2.eac28396.chunk.js"
+  },
+  {
+    "revision": "66a67ce241b04d832705",
+    "url": "/facerecognition-brain/static/js/main.0c8c829d.chunk.js"
+  },
+  {
+    "revision": "1b2deba2448aa9471f09",
+    "url": "/facerecognition-brain/static/js/runtime~main.b9073785.js"
+  }
+]);
