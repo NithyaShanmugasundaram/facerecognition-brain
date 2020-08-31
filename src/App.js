a406143980c6c2005175abaa943302443b874289
Ganesh kumar M <gunz@Ganeshs-MacBook-Pro.local>
2020-08-31 16:32:38 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 //import Rank from './components/Rank/Rank';
 import './App.css';
+//ddddsds
 
 
 const app = new Clarifai.App({
